3e109b42b897977c45eabf2e3f9152ae8e0e494f
Jiaming Nie <jiaming.nie13@gmail.com>
2019-02-19 21:05:09 -0500

    Create 1003.cpp

---
+++ Assignment1/1003.cpp
@@ -0,0 +1,18 @@
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+    double d;
+    cin>>d;
+    while(d > 0) {
+        int n = 0;
+        double l = 0.0;
+        while (l < d) {
+            n++;
+            l += 1.0 / (n+1);
+        }
+        printf("%d card(s)\n", n);
+        cin>>d;
+    }
+}
